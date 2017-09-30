54b14e557d0a4d66e808e94c593427cc64fd5b1d
eugenenew80 <eugenenew80@gmail.com>
2017-09-30 11:22:03 +0600

    -

--- src/main/java/kz/kegoc/bln/entity/adm/dto/RoleDictDto.java
+++ src/main/java/kz/kegoc/bln/entity/adm/dto/RoleDictDto.java
@@ -3,10 +3,12 @@ package kz.kegoc.bln.entity.adm.dto;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RoleDictDto {
 	private Long roleId;
 	private Long dictId;
--- src/main/java/kz/kegoc/bln/entity/adm/dto/RoleDto.java
+++ src/main/java/kz/kegoc/bln/entity/adm/dto/RoleDto.java
@@ -1,10 +1,12 @@
 package kz.kegoc.bln.entity.adm.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RoleDto {
 	private Long id;
 	private String code;
