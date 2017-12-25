083baa97981a5b7aa27ec90441b86c0a42848597
Eugene B <eugenenew80@gmail.com>
2017-12-25 20:33:45 +0600

    -

--- src/main/java/kz/kegoc/bln/ejb/cdi/Producer.java
+++ src/main/java/kz/kegoc/bln/ejb/cdi/Producer.java
@@ -4,32 +4,42 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import kz.kegoc.bln.entity.common.Lang;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
-import javax.enterprise.context.ApplicationScoped;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import javax.enterprise.inject.Produces;
 
-@ApplicationScoped
+@Singleton
+@Startup
 public class Producer {
     private RedissonClient redissonClient = null;
 
-    @Produces
-    public RedissonClient createRedissonClient() {
-        if (redissonClient !=null)
-            return redissonClient;
-
+    @PostConstruct
+    public void Producer() {
         ObjectMapper mapper = new ObjectMapper()
-            .registerModule(new ParameterNamesModule())
-            .registerModule(new Jdk8Module())
-            .registerModule(new JavaTimeModule());
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule());
 
         Config config = new Config();
         config.useSingleServer().setAddress("redis://127.0.0.1:6379");
         config.setCodec(new JsonJacksonCodec(mapper));
 
         redissonClient = Redisson.create(config);
+    }
+
+    @Produces
+    public RedissonClient getRedissonClient() {
         return redissonClient;
+    }
+
+    @Produces
+    public Lang defLang() {
+        return Lang.RU;
     }
 }
